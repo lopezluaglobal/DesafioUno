e807c20dbaa0cbb1c8e812514339de6d3823e8fd
lopezlua <lopezlua@globalhitss.com>
2024-10-09 21:34:54 -0600

    add desafios

---
+++ JavaScript/creaArregloRango.js
@@ -0,0 +1,12 @@
+inicio = 3;
+fin = 6;
+
+// Crear arreglo con números del rango
+let arreglo = [];
+for (let i = inicio; i <= fin; i++) {
+  arreglo.push(i);
+}
+
+// Imprimir arreglo
+console.log("Arreglo de números del rango:");
+console.log(arreglo);
